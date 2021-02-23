6ed207eb68b9d5ce191b7cc5b42258e0a1c3041b
Reagan Rogers <reagan422440@gmail.com>
2021-02-22 19:28:07 -0500

    Added variables for questions

---
+++ Quiz.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title: "NORMALLY used data types do not include?"
+        choices: ["string", "booleans","alerts", "Number"],
+        answer: "Alerts"
+    },
+    {
+        title: "In a function, your IF and ELSE statements MUST be contained _____??"
+        choices: ["quotes", "curly brackets","parenthesis", "Square brackets"],
+        answer: "Parenthesis"
+    },
+    {
+        title: "Javascript arrays can be used to hold....___?"
+        choices: ["Numbers and Strings", "Other Arrays ","Booleans", "Square brackets"],
+        answer: "All of the Above"
+    },
+    {
+        title: "String values must be enclosed within ___ when being assigned to variables?"
+        choices: ["Semicolins", "Curly Brackets","Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+    {
+        title: "a very useful tool used during development and debugging for printing content to the debugger is:"
+        choices: ["Javascript", "Termianal/Bash","Console log"],
+        answer: "Console log"
+    },
+];
