8cb2c54db068bdd97b6baa3797e0894117545007
rrogers422 <Reagan422440@gmail.com>
2021-06-22 13:24:01 -0400

    finishsed functions for quiz.js

--- Quiz.js
+++ Quiz.js
@@ -80,14 +80,98 @@ function compare(event) {
     if (element.matches("li")) {
         var createDiv = document.createElement("div");
         createDiv.setAttribute("id", "createDiv");
-        if(element.textContent == testQuestions[questionArea].answer){
+        if(element.textContent == testQuestions[questionIndex].answer){
             score++;
-            createDiv.textContent = "Corrct! The answer is: " + testQuestions[questionArea].answer;
+            createDiv.textContent = "Corrct! The answer is: " + testQuestions[questionIndex].answer;
         } else {
             //deducts -15 seconds off for wrong answers
             timeLeft = timeLeft - penalty;
-            createDiv.textContent = "Wrong! The correct answer is:" + testQuestions[questionArea].answer;
+            createDiv.textContent = "Wrong! The correct answer is:" + testQuestions[questionIndex].answer;
         }
         
     }
+}
+
+questionIndex++;
+if(questionIndex >= testQuestions.length) {
+    allDone();
+    createDiv.textContent = "Finished!" + " " + "You Got " + score + "/" + testQuestions.length + " Correct!";
+} else {
+    render(questionIndex);
+}
+questionArea.appendChild(createDiv);
+
+function allDone() {
+    questionArea.innerHTML = "";
+    currentTime.innerHTML = "";
+
+    var createH1 = document.createElement("h1");
+    createH1.setAttribute("id", "createH1");
+    createH1.textContent = "Finished!"
+
+    questionArea.appendChild(createH1);
+
+    var createP = document.createElement("p");
+    createP.setAttribute("id", "createP");
+
+    questionArea.appendChild(createP);
+
+    // Calculates time remaining and replaces it with score
+    if (timeLeft >= 0) {
+        var timeRemaining = timeLeft;
+        var createP2 = document.createElement("p");
+        clearInterval(interval);
+        createP.textContent = "final score: " + timeRemaining;
+
+        questionArea.appendChild(createP2);
+    }
+
+    var createLabel = document.createElement("label");
+    createLabel.setAttribute("id", "createLabel");
+    createLabel.textContent = "Enter initials: ";
+
+    questionArea.appendChild(createLabel);
+
+    var createInput = document.createElement("input");
+    createInput.setAttribute("type", "text");
+    createInput.setAttribute("id", "initials");
+    createInput.textContent = "";
+
+    questionArea.appendChild(createInput);
+
+    var createSubmit = document.createElement("button");
+    createSubmit.setAttribute("type", "submit");
+    createSubmit.setAttribute("id", "Submit");
+    createSubmit.textContent = "Submit";
+
+    questionArea.appendChild(createSubmit);
+
+    // Event listener to capture initials and local storage for initials and score
+    createSubmit.addEventListener("click", function () {
+        var initials = createInput.value;
+
+        if (initials === null) {
+
+            console.log("No value entered!");
+
+        } else {
+            var finalScore = {
+                initials: initials,
+                score: timeRemaining
+            }
+            console.log(finalScore);
+            var allScores = localStorage.getItem("allScores");
+            if (allScores === null) {
+                allScores = [];
+            } else {
+                allScores = JSON.parse(allScores);
+            }
+            allScores.push(finalScore);
+            var newScore = JSON.stringify(allScores);
+            localStorage.setItem("allScores", newScore);
+            // Takes user to highscores page
+            window.location.replace("./highscores.html");
+        }
+    });
+
 }
